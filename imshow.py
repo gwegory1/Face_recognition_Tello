4250f2adab427a7842d16b631786e11bff531cd0
gwegory1 <104769156+gwegory1@users.noreply.github.com>
2024-11-28 15:26:48 +0100

    Update imshow.py

--- imshow.py
+++ imshow.py
@@ -3,7 +3,7 @@ import cv2
 
 frame = cv2.imread('Screenshot from 2024-09-10 16-53-35.png')
 
-print('már meg kellett volna jelennie')
+print('show')
 
-cv2.imshow("Nézd meg hogy megy", frame)
+cv2.imshow("Frame", frame)
 cv2.waitKey(0)
